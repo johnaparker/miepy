ee899ea92c8b4d51f69828ea1c18fa2885c19fdf
John <japarker@uchicago.edu>
2018-02-20 23:05:03 -0600

    added performance test

---
+++ examples/tests/performance.py
@@ -0,0 +1,57 @@
+"""
+performance test
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import miepy
+from tqdm import tqdm
+
+from timeit import default_timer as timer
+import timeit
+
+
+nm = 1e-9
+
+Ag = miepy.materials.predefined.Ag()
+radius = 75*nm
+source = miepy.sources.rhc_polarized_plane_wave(amplitude=1)
+separation = 250*nm
+
+def force_func(N):
+    spheres = miepy.spheres([[n*separation, 0, 0] for n in range(N)], radius, Ag)
+    mie = miepy.gmt(spheres, source, 600*nm, 3)
+    
+    start = timer()
+    mie.force()
+    end = timer()
+
+    return end - start
+
+def flux_func(N):
+    spheres = miepy.spheres([[n*separation, 0, 0] for n in range(N)], radius, Ag)
+    mie = miepy.gmt(spheres, source, 600*nm, 1)
+    
+    start = timer()
+    mie.cross_sections()
+    end = timer()
+
+    return end - start
+
+def interactions_func(N):
+    spheres = miepy.spheres([[n*separation, 0, 0] for n in range(N)], radius, Ag)
+    mie = miepy.gmt(spheres, source, 600*nm, 1)
+    
+    start = timer()
+    mie._solve_interactions()
+    end = timer()
+
+    return end - start
+
+def N_particle_test(func, Nmax = 20):
+    print(func.__name__)
+    for N in range(1, Nmax+1):
+        time = func(N)
+        print('  ', f'N = {N}: ', f'{time:.3f}s')
+
+N_particle_test(force_func)
