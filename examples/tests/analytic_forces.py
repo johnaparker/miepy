752315fd1bf54a26ea082780a81144c39e078380
John <japarker@uchicago.edu>
2018-03-01 19:03:59 -0600

    analytic x and y forces implemented

--- examples/tests/analytic_forces.py
+++ examples/tests/analytic_forces.py
@@ -23,7 +23,7 @@ mie = miepy.gmt(spheres, source, 600*nm, 2)
 force = np.zeros((3,) + separations.shape)
 torque = np.zeros_like(force)
 
-def get_fz(self,i):
+def get_force(self,i):
     fid = 0
     k = self.material_data['k'][fid]
     radius = self.spheres.radius[fid]
@@ -32,22 +32,57 @@ def get_fz(self,i):
     mu_b = self.material_data['mu_b'][fid]
 
 
+    Fxy = 0
     Fz = 0
-    A = -2*np.pi*E0**2/k**2
+    Axy = np.pi*E0**2/k**2
+    Az = -2*np.pi*E0**2/k**2
 
     for n in range(1,self.Lmax+1):
         for m in range(-n,n+1):
             r = n**2 + n - 1 + m
-            factor = A*m*(eps_b/mu_b)/(n*(n+1))
 
+            # Fxy, term 1/3
+            if m != n:
+                factor = Axy*np.sqrt((n+m+1)*(n-m))*(eps_b/mu_b)/(n+1)
+                r1 = n**2 + n - 1 + m + 1
+                Fxy += factor*(2*self.p[fid,i,r]*np.conj(self.q[fid,i,r1]) \
+                         - self.p[fid,i,r]*np.conj(self.q_inc[fid,i,r1]) \
+                         - self.p_inc[fid,i,r]*np.conj(self.q[fid,i,r1]) \
+                         + 2*self.q[fid,i,r]*np.conj(self.p[fid,i,r1]) \
+                         - self.q[fid,i,r]*np.conj(self.p_inc[fid,i,r1]) \
+                         - self.q_inc[fid,i,r]*np.conj(self.p[fid,i,r1]))
+
+            # Fz, term 1/2
+            factor = Az*m*(eps_b/mu_b)/(n*(n+1))
             Fz += factor*(2*self.p[fid,i,r]*np.conj(self.q[fid,i,r]) \
                     - self.p[fid,i,r]*np.conj(self.q_inc[fid,i,r]) \
                     - self.p_inc[fid,i,r]*np.conj(self.q[fid,i,r]))
 
-            if n < self.Lmax:
-                factor = A/(n+1)*np.sqrt((n-m+1)*(n+m+1)*n*(n+2)/(2*n+3)/(2*n+1))
-                r1 = (n+1)**2 + (n+1) - 1 + m
 
+            if n < self.Lmax:
+                # Fxy, term 2/3
+                factor = -Axy*np.sqrt((n+m+2)*(n+m+1)*n*(n+2)/((2*n+3)*(2*n+1)))/(n+1)
+                r1 = (n+1)**2 + (n+1) - 1 + m + 1
+                Fxy += factor*(2*eps_b*self.p[fid,i,r]*np.conj(self.p[fid,i,r1]) \
+                         - eps_b*self.p[fid,i,r]*np.conj(self.p_inc[fid,i,r1]) \
+                         - eps_b*self.p_inc[fid,i,r]*np.conj(self.p[fid,i,r1]) \
+                         + 2*eps_b/mu_b*self.q[fid,i,r]*np.conj(self.q[fid,i,r1]) \
+                         - eps_b/mu_b*self.q[fid,i,r]*np.conj(self.q_inc[fid,i,r1]) \
+                         - eps_b/mu_b*self.q_inc[fid,i,r]*np.conj(self.q[fid,i,r1]))
+
+                # Fxy, term 3/3
+                factor = Axy*np.sqrt((n-m+1)*(n-m+2)*n*(n+2)/((2*n+3)*(2*n+1)))/(n+1)
+                r1 = (n+1)**2 + (n+1) - 1 + m - 1
+                Fxy += factor*(2*eps_b*self.p[fid,i,r1]*np.conj(self.p[fid,i,r]) \
+                         - eps_b*self.p[fid,i,r1]*np.conj(self.p_inc[fid,i,r]) \
+                         - eps_b*self.p_inc[fid,i,r1]*np.conj(self.p[fid,i,r]) \
+                         + 2*eps_b/mu_b*self.q[fid,i,r1]*np.conj(self.q[fid,i,r]) \
+                         - eps_b/mu_b*self.q[fid,i,r1]*np.conj(self.q_inc[fid,i,r]) \
+                         - eps_b/mu_b*self.q_inc[fid,i,r1]*np.conj(self.q[fid,i,r]))
+
+                # Fz, term 2/2
+                factor = Az/(n+1)*np.sqrt((n-m+1)*(n+m+1)*n*(n+2)/(2*n+3)/(2*n+1))
+                r1 = (n+1)**2 + (n+1) - 1 + m
                 Fz += factor*(2*eps_b*self.p[fid,i,r1]*np.conj(self.p[fid,i,r]) \
                         - eps_b*self.p[fid,i,r1]*np.conj(self.p_inc[fid,i,r]) \
                         - eps_b*self.p_inc[fid,i,r1]*np.conj(self.p[fid,i,r]) \
@@ -55,20 +90,43 @@ def get_fz(self,i):
                         - eps_b/mu_b*self.q[fid,i,r1]*np.conj(self.q_inc[fid,i,r]) \
                         - eps_b/mu_b*self.q_inc[fid,i,r1]*np.conj(self.q[fid,i,r]))
 
-    return np.real(Fz)
+
+
+    return np.array([np.real(Fxy), np.imag(Fxy), np.real(Fz)])
 
 for i, separation in enumerate(tqdm(separations)):
     mie.update_position(np.array([[separation/2,0,0], [-separation/2,0,0]]))
-    force[2,i] = get_fz(mie, 0)
+    force[:,i] = get_force(mie, 0)
 
-fig, ax = plt.subplots()
+
+fig, axes = plt.subplots(nrows=2, ncols=3, figsize=plt.figaspect(2/3)*2)
+
 with h5py.File('temp.h5', 'r') as f:
     F = f['F'][...]
-    ax.plot(separations/nm, F, 'o', label='Numerical Stress Tensor')
+    T = f['T'][...]
 
+    for i in range(3):
+        axes[0,i].plot(separations/nm, F[i], 'o', color=f'C{i}', label='Numerical Stress Tensor')
+        axes[1,i].plot(separations/nm, T[i], 'o', color=f'C{i}', label='Numerical Stress Tensor')
 
-ax.plot(separations/nm, force[2], label='Analytic Equation')
-ax.set(xlabel='separation (nm)', ylabel='force')
-plt.legend()
+for i in range(3):
+    comp = ['x', 'y', 'z'][i]
+
+    axes[0,i].plot(separations/nm, force[i], color=f'C{i}', label='Analytic Equation')
+    axes[0,i].set_title(label=f'F{comp}', weight='bold')
+
+    # axes[1,i].plot(separations/nm, torque[i], color=f'C{i}')
+    # axes[1,i].set_title(label=f'T{comp}', weight='bold')
+
+for ax in axes.flatten():
+    ax.axhline(y=0, color='black', linestyle='--')
+    ax.legend()
+
+for ax in axes[1]:
+    ax.set_xlabel('separation (nm)')
+
+axes[0,0].set_ylabel('force (N)')
+axes[1,0].set_ylabel('torque (mN)')
+
+plt.tight_layout()
 plt.show()
-
