9c537662a077de74eb92ee71b9a07fa1a8bb4185
John <japarker@uchicago.edu>
2018-04-03 02:15:56 -0500

    added vacuum to predefined materials

--- miepy/materials/predefined.py
+++ miepy/materials/predefined.py
@@ -1,14 +1,18 @@
 import miepy
 from miepy.materials.load import load_material
 
+### mediums
+def vacuum():
+    return miepy.constant_material(1)
+
+def water():
+    return miepy.constant_material(1.33**2)
+
+### metals
 def Ag(author='Johnson'):
     """Return silver material from MiePy data"""
     return load_material("Ag", author)
-    # return load_material(miepy.__path__[0] + "/materials/ag.npy")
 
 def Au(author='Johnson'):
     """Return gold material from MiePy data"""
     return load_material("Au", author)
-
-def water():
-    return miepy.constant_material(1.33**2)
