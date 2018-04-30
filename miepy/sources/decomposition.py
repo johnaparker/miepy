e6771f37ca6f3c99b3a03f8a89c141a36d184e20
John <japarker@uchicago.edu>
2018-04-30 17:49:42 -0500

    fixed bug in point matching decomposition

--- miepy/sources/decomposition.py
+++ miepy/sources/decomposition.py
@@ -24,6 +24,8 @@ def sample_sphere_point_matching(position, radius, sampling):
     return np.reshape(np.array([X,Y,Z]), [3, -1])[:,Nphi-1:-Nphi+1]
 
 #TODO: create a root function: instead of source... pass in raw fields for more generality, (no position needed)
+#TODO: sampling default based on value of Lmax
+#TODO: radius default based on value of k
 def point_matching(source, position, radius, k, Lmax, sampling=6):
     """Decompose a source into VSHs using the point matching method
        Returns p_src[2,rmax]
@@ -41,12 +43,14 @@ def point_matching(source, position, radius, k, Lmax, sampling=6):
     X = points[0]
     Y = points[1]
     Z = points[2]
-    _, THETA, PHI = miepy.coordinates.cart_to_sph(X, Y, Z)
+    _, THETA, PHI = miepy.coordinates.cart_to_sph(X, Y, Z, origin=position)
 
     rmax = miepy.vsh.Lmax_to_rmax(Lmax)
 
     E_src = source.E_field(X, Y, Z, k)
+    H_src = source.H_field(X, Y, Z, k)
     E_src = miepy.coordinates.vec_cart_to_sph(E_src, THETA, PHI)
+    H_src = miepy.coordinates.vec_cart_to_sph(H_src, THETA, PHI)
 
     E_vsh = np.zeros([3, Npoints, 2, rmax], dtype=complex)
     for i,(n,m) in enumerate(miepy.vsh.mode_indices(Lmax)):
@@ -55,8 +59,10 @@ def point_matching(source, position, radius, k, Lmax, sampling=6):
         E_vsh[...,0,i] = -1j*Emn_val*Nfunc(radius, THETA, PHI, k)
         E_vsh[...,1,i] = -1j*Emn_val*Mfunc(radius, THETA, PHI, k)
 
-    column = E_src.reshape([3*Npoints])
-    matrix = E_vsh.reshape([3*Npoints, 2*rmax])
+    H_vsh = -1j*E_vsh[...,::-1,:]
+
+    column = np.concatenate([E_src.reshape([-1]), H_src.reshape([-1])])
+    matrix = np.concatenate([E_vsh.reshape([-1, 2*rmax]), H_vsh.reshape([-1, 2*rmax])])
     sol = np.linalg.lstsq(matrix, column, rcond=None)
     p_src = sol[0]
 
