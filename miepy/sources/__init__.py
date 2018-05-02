4588cb4c8cbecc5b383f10bd504dfa156e02898e
John <japarker@uchicago.edu>
2018-05-01 22:50:10 -0500

    moved beams -> paraxial_beams

--- miepy/sources/__init__.py
+++ miepy/sources/__init__.py
@@ -1,5 +1,5 @@
 from . import plane_waves
-from . import beams
+from . import paraxial_beams
 from . import decomposition
 
 from .source_base import source
@@ -7,7 +7,7 @@ from .source_base import source
 from .plane_waves import (plane_wave, x_polarized_plane_wave, y_polarized_plane_wave,
                          rhc_polarized_plane_wave, lhc_polarized_plane_wave)
 
-from .beams import (gaussian_beam, hermite_gaussian_beam, laguerre_gaussian_beam,
+from .paraxial_beams import (gaussian_beam, hermite_gaussian_beam, laguerre_gaussian_beam,
                     azimuthal_beam, radial_beam, shear_beam)
 
 from .point import point_dipole
