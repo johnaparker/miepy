3701fad24ab12f5758637848e208c76332c62eeb
John <japarker@uchicago.edu>
2018-05-04 18:37:35 -0500

    introduced another symmetry relationship in the interaction matrix

--- miepy/interactions.py
+++ miepy/interactions.py
@@ -34,6 +34,8 @@ def sphere_cluster_t_matrix(positions, a, k):
 
             for r,n,m in miepy.mode_indices(Lmax):
                 for s,v,u in miepy.mode_indices(Lmax):
+                    if s - 2*u < r: continue
+
                     A_transfer, B_transfer = miepy.vsh.vsh_translation(m, n, u, v, 
                             r_ji, theta_ji, phi_ji, k, miepy.VSH_mode.outgoing)
 
@@ -42,10 +44,20 @@ def sphere_cluster_t_matrix(positions, a, k):
                     interaction_matrix[i,1,r,j,0,s] = B_transfer*a[j,0,v-1]
                     interaction_matrix[i,1,r,j,1,s] = A_transfer*a[j,1,v-1]
 
-                    interaction_matrix[j,0,r,i,0,s] = (-1)**(m+u)*A_transfer*a[i,0,v-1]
-                    interaction_matrix[j,0,r,i,1,s] = (-1)**(m+u)*B_transfer*a[i,1,v-1]
-                    interaction_matrix[j,1,r,i,0,s] = (-1)**(m+u)*B_transfer*a[i,0,v-1]
-                    interaction_matrix[j,1,r,i,1,s] = (-1)**(m+u)*A_transfer*a[i,1,v-1]
+                    interaction_matrix[j,0,r,i,0,s] = (-1)**(n+v)*A_transfer*a[i,0,v-1]
+                    interaction_matrix[j,0,r,i,1,s] = (-1)**(n+v+1)*B_transfer*a[i,1,v-1]
+                    interaction_matrix[j,1,r,i,0,s] = (-1)**(n+v+1)*B_transfer*a[i,0,v-1]
+                    interaction_matrix[j,1,r,i,1,s] = (-1)**(n+v)*A_transfer*a[i,1,v-1]
+
+                    interaction_matrix[i,0,s-2*u,j,0,r-2*m] = (-1)**(m+u)*A_transfer*a[j,0,n-1]
+                    interaction_matrix[i,0,s-2*u,j,1,r-2*m] = (-1)**(m+u+1)*B_transfer*a[j,1,n-1]
+                    interaction_matrix[i,1,s-2*u,j,0,r-2*m] = (-1)**(m+u+1)*B_transfer*a[j,0,n-1]
+                    interaction_matrix[i,1,s-2*u,j,1,r-2*m] = (-1)**(m+u)*A_transfer*a[j,1,n-1]
+
+                    interaction_matrix[j,0,s-2*u,i,0,r-2*m] = (-1)**(m+u+n+v)*A_transfer*a[i,0,n-1]
+                    interaction_matrix[j,0,s-2*u,i,1,r-2*m] = (-1)**(m+u+n+v)*B_transfer*a[i,1,n-1]
+                    interaction_matrix[j,1,s-2*u,i,0,r-2*m] = (-1)**(m+u+n+v)*B_transfer*a[i,0,n-1]
+                    interaction_matrix[j,1,s-2*u,i,1,r-2*m] = (-1)**(m+u+n+v)*A_transfer*a[i,1,n-1]
 
     t_matrix = identity + interaction_matrix
     return t_matrix
--- tests/test_vsh_translation.py
+++ tests/test_vsh_translation.py
@@ -26,21 +26,29 @@ for r,n,m in miepy.mode_indices(Lmax):
                 r_ji, np.pi - theta_ji, np.pi + phi_ji, k, miepy.vsh.VSH_mode.outgoing)
         A_expect, B_expect = (-1)**(n+v)*A, (-1)**(n+v+1)*B
 
-        # assert np.allclose(A_calc, A_expect)
-        # assert np.allclose(B_calc, B_expect)
+        assert np.allclose(A_calc, A_expect)
+        assert np.allclose(B_calc, B_expect)
 
         ### swap (m,n) with (u,v) and negate m and u
         A_calc, B_calc = miepy.vsh.vsh_translation(-u, v, -m, n, 
                 r_ji, theta_ji, phi_ji, k, miepy.vsh.VSH_mode.outgoing)
         A_expect, B_expect = (-1)**(m+u)*A, (-1)**(m+u+1)*B
 
-        # assert np.allclose(A_calc, A_expect)
-        # assert np.allclose(B_calc, B_expect)
+        assert np.allclose(A_calc, A_expect)
+        assert np.allclose(B_calc, B_expect)
+
+        ### swap (m,n) with (u,v) and negate m and u and reverse positions
+        A_calc, B_calc = miepy.vsh.vsh_translation(-u, v, -m, n, 
+                r_ji, np.pi - theta_ji, np.pi + phi_ji, k, miepy.vsh.VSH_mode.outgoing)
+        A_expect, B_expect = (-1)**(m+u+n+v)*A, (-1)**(m+u+n+v)*B
+
+
+        assert np.allclose(A_calc, A_expect)
+        assert np.allclose(B_calc, B_expect)
 
         ### swap (m,n) with (u,v)
         A_calc, B_calc = miepy.vsh.vsh_translation(u, v, m, n, 
-                r_ji, np.pi - theta_ji, np.pi + phi_ji, k, miepy.vsh.VSH_mode.outgoing)
-        factor = (-1)**(n+v) #*factorial(n-m)*factorial(v+u)/(factorial(n+m)*factorial(v-u))
-        A_expect, B_expect = factor*A, factor*B
+                r_ji, theta_ji, phi_ji, k, miepy.vsh.VSH_mode.outgoing)
+        A_expect, B_expect = (-1)**(m+u)*A, (-1)**(m+u+1)*B
 
         print(f'{(n,m)} -> {(v,u)}: A = {A_calc}, Ap = {A_expect}, B = {B_calc}, Bp = {B_expect}')
