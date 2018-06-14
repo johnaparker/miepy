18c6f00f239464a17d772a2d93ba6c2734c46763
John <japarker@uchicago.edu>
2018-06-13 19:30:10 -0500

    renamed paraxial_beams -> beams

--- miepy/sources/__init__.py
+++ miepy/sources/__init__.py
@@ -7,7 +7,7 @@ from .source_base import source
 from .plane_waves import (plane_wave, x_polarized_plane_wave, y_polarized_plane_wave,
                          rhc_polarized_plane_wave, lhc_polarized_plane_wave)
 
-from .paraxial_beams import (gaussian_beam, hermite_gaussian_beam, laguerre_gaussian_beam,
+from .beams import (gaussian_beam, hermite_gaussian_beam, laguerre_gaussian_beam,
                     paraxial_beam, bigaussian_beam, azimuthal_beam, radial_beam, shear_beam)
 
 from .point import point_dipole
