718dd411535d6275049816cb2ee86add16d1b8bf
John <japarker@uchicago.edu>
2018-06-28 15:21:01 -0500

    performance changes brought to particle aggregates

--- miepy/interactions.py
+++ miepy/interactions.py
@@ -25,23 +25,18 @@ def solve_linear_system(tmatrix, p_src, method):
         sol = bicgstab(tmatrix.reshape((2*N*rmax, -1)), p_src.reshape((-1,)))[0]
         return sol.reshape((N,2,rmax))
 
-def sphere_aggregate_tmatrix(positions, a, k):
-    """Obtain the particle-centered aggregate T-matrix for a cluster of spheres
-       Returns T[N,2,rmax,N,2,rmax]
-    
+def interactions_precomputation(positions, k, lmax):
+    """Get the relative r,theta,phi positions of the particles and precomputed zn function
+
        Arguments:
            positions[N,3]      particles positions
-           a[N,2,lmax]         mie scattering coefficients
-           k                   medium wavenumber
+           k      medium wavenumber
+           lmax   maximum number of multipoles
+    
+       Returns:
+           r[idx], theta[idx], phi[idx], zn[nmax,idx] (idx enumerates i,j and j > i)
     """
     Nparticles = positions.shape[0]
-    lmax = a.shape[-1]
-    rmax = miepy.vsh.lmax_to_rmax(lmax)
-    identity = np.zeros(shape = (Nparticles, 2, rmax, Nparticles, 2, rmax), dtype=complex)
-    np.einsum('airair->air', identity)[...] = 1
-    
-    interaction_matrix = np.zeros(shape = (Nparticles, 2, rmax, Nparticles, 2, rmax), dtype=complex)
-
     size = int(Nparticles*(Nparticles-1)/2)
     r_ji     = np.zeros(size, dtype=float)
     theta_ji = np.zeros(size, dtype=float)
@@ -63,6 +58,26 @@ def sphere_aggregate_tmatrix(positions, a, k):
     zn_values = np.zeros((nmax, size), dtype=complex)
     for n in range(nmax):
         zn_values[n] = miepy.vsh.special.spherical_hn(n, k*r_ji)
+
+    return r_ji, theta_ji, phi_ji, zn_values
+
+def sphere_aggregate_tmatrix(positions, a, k):
+    """Obtain the particle-centered aggregate T-matrix for a cluster of spheres
+       Returns T[N,2,rmax,N,2,rmax]
+    
+       Arguments:
+           positions[N,3]      particles positions
+           a[N,2,lmax]         mie scattering coefficients
+           k                   medium wavenumber
+    """
+    Nparticles = positions.shape[0]
+    lmax = a.shape[-1]
+    rmax = miepy.vsh.lmax_to_rmax(lmax)
+    identity = np.zeros(shape = (Nparticles, 2, rmax, Nparticles, 2, rmax), dtype=complex)
+    np.einsum('airair->air', identity)[...] = 1
+    
+    interaction_matrix = np.zeros(shape = (Nparticles, 2, rmax, Nparticles, 2, rmax), dtype=complex)
+    r_ji, theta_ji, phi_ji, zn_values = interactions_precomputation(positions, k, lmax)
 
     for r,n,m in miepy.mode_indices(lmax):
         for s,v,u in miepy.mode_indices(lmax):
@@ -96,8 +111,8 @@ def sphere_aggregate_tmatrix(positions, a, k):
             interaction_matrix[:,:,r,:,:,s] *= a[:,:,v-1]
             # interaction_matrix[:,:,s-2*u,:,:,r-2*m] *= a[:,:,n-1]
 
-    t_matrix = identity + interaction_matrix
-    return t_matrix
+    agg_tmatrix = identity + interaction_matrix
+    return agg_tmatrix
 
 #TODO this function is more general than above and can be used for both cases (change only the einsum)
 def particle_aggregate_tmatrix(positions, tmatrix, k):
@@ -116,43 +131,38 @@ def particle_aggregate_tmatrix(positions, tmatrix, k):
     np.einsum('airair->air', identity)[...] = 1
     
     interaction_matrix = np.zeros(shape = (Nparticles, 2, rmax, Nparticles, 2, rmax), dtype=complex)
+    r_ji, theta_ji, phi_ji, zn_values = interactions_precomputation(positions, k, lmax)
 
-    for i in range(Nparticles):
-        for j in range(i+1, Nparticles):
-            pi = positions[i]
-            pj = positions[j]
-            dji = pi -  pj
-            r_ji = np.linalg.norm(dji)
-            theta_ji = np.arccos(dji[2]/r_ji)
-            phi_ji = np.arctan2(dji[1], dji[0])
+    for r,n,m in miepy.mode_indices(lmax):
+        for s,v,u in miepy.mode_indices(lmax):
+            # if s - 2*u < r: continue
+            A_transfer, B_transfer = miepy.vsh.vsh_translation(m, n, u, v, 
+                    r_ji, theta_ji, phi_ji, k, miepy.VSH_mode.outgoing, zn_values=zn_values)
 
-            for r,n,m in miepy.mode_indices(lmax):
-                for s,v,u in miepy.mode_indices(lmax):
-                    if s - 2*u < r: continue
+            upper_idx = np.triu_indices(Nparticles, 1)
+            lower_idx = upper_idx[::-1]
 
-                    A_transfer, B_transfer = miepy.vsh.vsh_translation(m, n, u, v, 
-                            r_ji, theta_ji, phi_ji, k, miepy.VSH_mode.outgoing)
+            interaction_matrix[:,0,r,:,0,s][upper_idx] = A_transfer
+            interaction_matrix[:,0,r,:,1,s][upper_idx] = B_transfer
+            interaction_matrix[:,1,r,:,0,s][upper_idx] = B_transfer
+            interaction_matrix[:,1,r,:,1,s][upper_idx] = A_transfer
 
-                    interaction_matrix[i,0,r,j,0,s] = A_transfer
-                    interaction_matrix[i,0,r,j,1,s] = B_transfer
-                    interaction_matrix[i,1,r,j,0,s] = B_transfer
-                    interaction_matrix[i,1,r,j,1,s] = A_transfer
+            interaction_matrix[:,0,r,:,0,s][lower_idx] = (-1)**(n+v)  *A_transfer
+            interaction_matrix[:,0,r,:,1,s][lower_idx] = (-1)**(n+v+1)*B_transfer
+            interaction_matrix[:,1,r,:,0,s][lower_idx] = (-1)**(n+v+1)*B_transfer
+            interaction_matrix[:,1,r,:,1,s][lower_idx] = (-1)**(n+v)  *A_transfer
 
-                    interaction_matrix[j,0,r,i,0,s] = (-1)**(n+v)*A_transfer
-                    interaction_matrix[j,0,r,i,1,s] = (-1)**(n+v+1)*B_transfer
-                    interaction_matrix[j,1,r,i,0,s] = (-1)**(n+v+1)*B_transfer
-                    interaction_matrix[j,1,r,i,1,s] = (-1)**(n+v)*A_transfer
+            # interaction_matrix[:,0,s-2*u,:,0,r-2*m][upper_idx] = (-1)**(m+u)  *A_transfer
+            # interaction_matrix[:,0,s-2*u,:,1,r-2*m][upper_idx] = (-1)**(m+u+1)*B_transfer
+            # interaction_matrix[:,1,s-2*u,:,0,r-2*m][upper_idx] = (-1)**(m+u+1)*B_transfer
+            # interaction_matrix[:,1,s-2*u,:,1,r-2*m][upper_idx] = (-1)**(m+u)  *A_transfer
 
-                    interaction_matrix[i,0,s-2*u,j,0,r-2*m] = (-1)**(m+u)*A_transfer
-                    interaction_matrix[i,0,s-2*u,j,1,r-2*m] = (-1)**(m+u+1)*B_transfer
-                    interaction_matrix[i,1,s-2*u,j,0,r-2*m] = (-1)**(m+u+1)*B_transfer
-                    interaction_matrix[i,1,s-2*u,j,1,r-2*m] = (-1)**(m+u)*A_transfer
-
-                    interaction_matrix[j,0,s-2*u,i,0,r-2*m] = (-1)**(m+u+n+v)*A_transfer
-                    interaction_matrix[j,0,s-2*u,i,1,r-2*m] = (-1)**(m+u+n+v)*B_transfer
-                    interaction_matrix[j,1,s-2*u,i,0,r-2*m] = (-1)**(m+u+n+v)*B_transfer
-                    interaction_matrix[j,1,s-2*u,i,1,r-2*m] = (-1)**(m+u+n+v)*A_transfer
+            # interaction_matrix[:,0,s-2*u,:,0,r-2*m][lower_idx] = (-1)**(m+u+n+v)*A_transfer
+            # interaction_matrix[:,0,s-2*u,:,1,r-2*m][lower_idx] = (-1)**(m+u+n+v)*B_transfer
+            # interaction_matrix[:,1,s-2*u,:,0,r-2*m][lower_idx] = (-1)**(m+u+n+v)*B_transfer
+            # interaction_matrix[:,1,s-2*u,:,1,r-2*m][lower_idx] = (-1)**(m+u+n+v)*A_transfer
 
     interaction_matrix = np.einsum('iabjcd,jcdef->iabjef', interaction_matrix, tmatrix)
-    A = identity + interaction_matrix
-    return A
+    agg_tmatrix = identity + interaction_matrix
+
+    return agg_tmatrix
