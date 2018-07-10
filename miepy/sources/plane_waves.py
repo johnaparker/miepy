bug fix in T-matrix rotation

miepy/sources/plane_waves.py
@@ -129,7 +129,7 @@ class plane_wave(source):
                       # (pi_value*(self.polarization[0]*np.cos(self.phi) + self.polarization[1]*np.cos(self.phi - np.pi/2))) \
                 # + 1j*(tau_value*(self.polarization[0]*np.sin(self.phi) + self.polarization[1]*np.sin(self.phi - np.pi/2))))
 
-        if self.theta != 0 and self.phi != 0:
+        if self.theta != 0 or self.phi != 0:
             quat = quaternion.from_spherical_coords(self.theta, self.phi)
             p_src = miepy.vsh.rotate_expansion_coefficients(p_src, quat)
 

miepy/tmatrix/functions.py
@@ -41,11 +41,13 @@ def rotate_tmatrix(tmatrix, quat):
     R = np.zeros([rmax, rmax], dtype=complex)
 
     for i,n,m in miepy.mode_indices(lmax):
-        # remove this for loop, fill in R block diagonal style
+        #TODO: remove this for loop, fill in R block diagonal style
         for j,v,u in miepy.mode_indices(lmax):
             if n == v:
                 R[i,j] = miepy.vsh.vsh_rotation.wigner_D(n, m, u, alpha, beta, gamma) 
 
-    tmatrix_rot = np.einsum('sm,wu,asbw->ambu', R, np.conjugate(R.T), tmatrix)
+    #TODO: which should be correct?
+    # tmatrix_rot = np.einsum('sm,wu,asbw->ambu', R, np.conjugate(R), tmatrix)
+    tmatrix_rot = np.einsum('sm,wu,ambu->asbw', R, np.conjugate(R), tmatrix)
 
     return tmatrix_rot