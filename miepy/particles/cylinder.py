14ebe11380a2e3627b10e4268879ed3fe1c0174e
John <japarker@uchicago.edu>
2018-09-26 01:43:25 -0500

    fixed cylinder tmatrix computation

--- miepy/particles/cylinder.py
+++ miepy/particles/cylinder.py
@@ -3,8 +3,7 @@ import numpy as np
 from .particle_base import particle
 
 class cylinder(particle):
-    def __init__(self, position, radius, height, material, orientation=None, rounded=False, extended_precision=False, Nint=200,
-            lmax=None):
+    def __init__(self, position, radius, height, material, orientation=None, rounded=False, extended_precision=False, Nint=200, tmatrix_lmax=0):
         """A cylinder object
 
         Arguments:
@@ -20,23 +19,22 @@ class cylinder(particle):
         self.extended_precision = extended_precision
         self.Nint = 200
         self.rounded = rounded
-        self.lmax = lmax
+
+        self.tmatrix_lmax = tmatrix_lmax
 
     def is_inside(self, pos):
         pass
 
     def compute_tmatrix(self, lmax, wavelength, eps_m, **kwargs):
-        if self.lmax is None:
-            lmax_compute = lmax
-        else:
-            lmax_compute = self.lmax
+        calc_lmax = max(lmax+2, self.tmatrix_lmax)
 
-        self.tmatrix = miepy.tmatrix.tmatrix_cylinder(self.radius, self.height, wavelength, 
-                self.material.eps(wavelength), eps_m, lmax_compute, rounded=self.rounded, extended_precision=self.extended_precision,
-                Nint=self.Nint)
+        self.tmatrix_fixed = miepy.tmatrix.tmatrix_cylinder(self.radius, self.height, wavelength, 
+                self.material.eps(wavelength), eps_m, calc_lmax, rounded=self.rounded, extended_precision=self.extended_precision, Nint=self.Nint)
 
-        if self.lmax is not None:
-            self.tmatrix = miepy.tmatrix.tmatrix_reduce_lmax(self.tmatrix, lmax)
+        if lmax < calc_lmax:
+            self.tmatrix_fixed = miepy.tmatrix.tmatrix_reduce_lmax(self.tmatrix_fixed, lmax)
+
+        self._rotate_fixed_tmatrix()
 
         return self.tmatrix
 
