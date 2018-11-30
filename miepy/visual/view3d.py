522eb5a981c1c3d38953fa8c4a38a1a0b8f9f924
John <japarker@uchicago.edu>
2018-11-29 18:33:45 -0600

    visualization: more improvements

--- miepy/visual/view3d.py
+++ miepy/visual/view3d.py
@@ -4,20 +4,21 @@ import numpy as np
 from itertools import count
 import matplotlib.colors as cm
 
-def get_texture(material):
-    """Return a (texture, color) tuple for a given material"""
+def get_paint(material):
+    """Return a (texture, color, shininess) tuple for a given material"""
     import vpython
     paint = namedtuple('paint', ['texture', 'color', 'shininess'])
 
     if material.name == 'Ag':
         return paint(vpython.textures.metal, vpython.vec(*[.9]*3), shininess=0.15)
     if material.name == 'Au':
-        return paint(vpython.textures.metal, vpython.vec(255/255, 210/255, 20/255), shininess=0.10)
+        return paint(vpython.textures.metal, vpython.vec(255/255, 210/255, 0/255), shininess=0.07)
 
     default_color = vpython.vec(*cm.to_rgb('C0'))
     return paint(None, default_color, shininess=1)
 
 def draw_particle(particle, paint, **kwargs):
+    """Draw a (non-spherical) particle with a given paint"""
     import vpython
     vec = vpython.vec
 
@@ -53,10 +54,9 @@ def draw_particle(particle, paint, **kwargs):
                                     color=paint.color,
                                     shininess=paint.shininess, **kwargs)
 
-#TODO: animation: pan camera around (widgets for rotation speed, transparency, pause, play, stop)
 #TODO: scale bar: auto-detect position and size
 #TODO: center: COM + FOV. Options: 'origin', 'auto', np.array
-def visualize(cluster, animation=False, transparent=False, scale=None, center='auto'):
+def visualize(cluster, animation=False, transparent=False, scale=None, origin='auto'):
     """
     Create a 3D visualization of a particle cluster using VPython
     
@@ -65,25 +65,33 @@ def visualize(cluster, animation=False, transparent=False, scale=None, center='a
         animation    (bool) an animation visualization
         transparent  (bool) transparent particles
         scale        (str) if set, display a scale bar
-        origin       origin of camera view
+        origin       origin of camera view ('auto' for centroid, 'cluster' for cluster origin, or np.array)
     """
     import vpython
     vec = vpython.vec
     # scene = vpython.canvas(width=750, height=600, background=vec(1,1,1))
-    scene = vpython.canvas(width=500, height=400, background=vec(1,1,1))
+
+    if origin == 'auto':
+        origin = vec(*np.average(cluster.position, axis=0))
+    elif origin == 'cluster':
+        origin = vec(*cluster.origin)
+    else:
+        origin = vec(*origin)
+
+    scene = vpython.canvas(width=500, height=325, background=vec(1,1,1), center=origin)
 
     if type(cluster) == miepy.sphere_cluster:
         for i in range(cluster.Nparticles):
             position = cluster.position[i]
             radius = cluster.radius[i]
-            paint = get_texture(cluster.material[i])
+            paint = get_paint(cluster.material[i])
 
             sphere = vpython.sphere(pos=vec(*position), radius=radius, texture=paint.texture, color=paint.color, shininess=paint.shininess)
 
     elif type(cluster) == miepy.cluster:
         for i in range(cluster.Nparticles):
             particle = cluster.particles[i]
-            paint = get_texture(particle.material)
+            paint = get_paint(particle.material)
 
             obj = draw_particle(particle, paint)
 
@@ -96,11 +104,48 @@ def visualize(cluster, animation=False, transparent=False, scale=None, center='a
         # cluster = vpython.compound(scene.objects)
         # cluster.rotate(angle=2*np.pi/T, origin=vec(0,0,0), axis=vec(0,1,0))
 
+        scene.append_to_caption('\n')
+
+        def setspeed(s):
+            nonlocal T
+            wt.text = 'f = {:1.2f} Hz'.format(s.value)
+            T = 30/s.value
+            
+        sl = vpython.slider(min=0.01, max=.3, value=.1, length=180, bind=setspeed, right=15, top=7)
+        wt = vpython.wtext(text='f = {:1.2f} Hz'.format(sl.value))
+
+        def transparency(b):
+            if b.checked:
+                for obj in scene.objects:
+                    obj.opacity = .5
+            else:
+                for obj in scene.objects:
+                    obj.opacity = 1
+
+        scene.append_to_caption('    ')
+        vpython.checkbox(bind=transparency, text='Transparent')
+
+        running = True
+        def Run(b):
+            nonlocal running
+            running = not running
+            if running:
+                b.text = " ▌▌"
+                b.color = vec(1,0,0)
+            else:
+                b.text = "  ►  "
+                b.color = vec(0,.6,0)
+
+        scene.append_to_caption('       ')
+        vpython.button(text=" ▌▌", bind=Run, color=vec(1,0,0))
+
+
         for t in count():
             vpython.rate(30)
 
             # phi = 2*np.pi*t/T
             # scene.forward = vec(-np.sin(phi), 0, -np.cos(phi))
             
-            for obj in scene.objects:
-                obj.rotate(angle=2*np.pi/T, origin=vec(0,0,0), axis=vec(0,1,0))
+            if running:
+                for obj in scene.objects:
+                    obj.rotate(angle=2*np.pi/T, origin=origin, axis=vec(0,1,0))
