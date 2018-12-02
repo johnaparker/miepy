581d4b2d71eea09b438a1554fbcf44020ad36cab
John <japarker@uchicago.edu>
2018-12-02 17:34:15 -0600

    new predefined materials

--- miepy/materials/predefined.py
+++ miepy/materials/predefined.py
@@ -23,3 +23,27 @@ def Au(author='Johnson'):
 def Al(author='Rakic'):
     """Return aluminum material from MiePy data"""
     return load_material("Al", author)
+
+def Ni(author='Johnson'):
+    """Return Nickel material from MiePy data"""
+    return load_material("Ni", author)
+
+def Cu(author='Johnson'):
+    """Return Copper material from MiePy data"""
+    return load_material("Cu", author)
+
+def Co(author='Johnson'):
+    """Return Cobalt material from MiePy data"""
+    return load_material("Co", author)
+
+def Pt(author='Rakic-LD'):
+    """Return Platinum material from MiePy data"""
+    return load_material("Pt", author)
+
+def TiO2(author='Siefke'):
+    """Return TiO2 material from MiePy data"""
+    return load_material("TiO2", author)
+
+def silica(author='Gao'):
+    """Return silica material from MiePy data"""
+    return load_material("SiO2", author)
