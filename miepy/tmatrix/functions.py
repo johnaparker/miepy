bug fix in VSH rotation matrix

miepy/tmatrix/functions.py
@@ -40,8 +40,8 @@ def rotate_tmatrix(tmatrix, quat):
     for i,n,m in miepy.mode_indices(lmax):
         r = miepy.vsh.lmax_to_rmax(n)
         idx = np.s_[r-(2*n+1):r]
-        R[idx,idx] = miepy.vsh.vsh_rotation_matrix(n, quat).T
+        R[idx,idx] = miepy.vsh.vsh_rotation_matrix(n, quat)
 
-    tmatrix_rot = np.einsum('sm,wu,asbw->ambu', R, np.conjugate(R), tmatrix)
+    tmatrix_rot = np.einsum('ms,uw,asbw->ambu', R, np.conjugate(R), tmatrix)
 
     return tmatrix_rot

miepy/vsh/vsh_rotation.py
@@ -33,7 +33,10 @@ def vsh_rotation_matrix(n, quat):
     l = 2*n + 1
     R = Wigner_D_matrices(quat, n, n).reshape((l,l))
 
-    return R
+    m = np.arange(-n, n+1)
+    R *= np.power(-1.0, np.subtract.outer(m, m))
+
+    return np.conj(R)
 
 def rotate_expansion_coefficients(p_exp, quat):
     """Rotate a set of expansion coefficients to a new reference frame
@@ -53,6 +56,6 @@ def rotate_expansion_coefficients(p_exp, quat):
         R = vsh_rotation_matrix(n, quat)
         rmax = miepy.vsh.lmax_to_rmax(n)
         idx = np.s_[rmax-(2*n+1):rmax]
-        p_rot[:,idx] = np.einsum('ab,pa->pb', R, p_exp[:,idx])
+        p_rot[:,idx] = np.einsum('ab,pb->pa', R, p_exp[:,idx])
 
     return p_rot

tests/test_tmatrix.py
@@ -51,7 +51,6 @@ def test_rotated_spheroid_equals_rotated_light():
 
     theta = 1.3
     phi = 0.7
-    q = miepy.quaternion.from_spherical_coords(theta, phi)
 
     source = miepy.sources.plane_wave([1,0], theta=theta, phi=phi)
     z_oriented = miepy.cluster(particles=miepy.spheroid([0,0,0], radius, 2*radius, material),
@@ -63,6 +62,7 @@ def test_rotated_spheroid_equals_rotated_light():
     C1 = z_oriented.cross_sections()
 
     source = miepy.sources.plane_wave([1,0])
+    q = miepy.quaternion.from_spherical_coords(theta, phi).inverse()
     oriented = miepy.cluster(particles=miepy.spheroid([0,0,0], radius, 2*radius, material, orientation=q),
                              source=source,
                              wavelength=wavelength,
@@ -71,4 +71,4 @@ def test_rotated_spheroid_equals_rotated_light():
 
     C2 = oriented.cross_sections()
 
-    assert np.allclose(C1, C2, rtol=0, atol=5e-30)
+    assert np.allclose(C1, C2, rtol=0, atol=7e-29)