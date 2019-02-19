abc12808cb42f0f6d82dbd5b853ce3a58c217eb2
John <japarker@uchicago.edu>
2019-02-18 20:51:12 -0600

    microscope improvements

--- miepy/microscope/microscope.py
+++ miepy/microscope/microscope.py
@@ -4,28 +4,38 @@ import miepy
 
 class microscope:
     """A microscope to produce images of a cluster"""
-    def __init__(self, cluster, focal_length, theta_obj=np.pi/2, sampling=30):
+    def __init__(self, cluster, medium=None, focal_img=100, focal_obj=1, theta_obj=np.pi/2, sampling=30):
         """
         Arguments:
             cluster        miepy cluster
-            focal_length   focal length of the microscope
-            theta_obj      maximum collection angle of the collection objective
+            medium         the outer medium of the microscope (default: air)
+            focal_img      focal length of the imaging lens (default: 100)
+            focal_obj      focal length of the objective lens (default: 100)
+            theta_obj      maximum collection angle of the objective lens
             sampling       far-field sampling
         """
         self.cluster = cluster
-        self.focal_length = focal_length
+        self.focal_img = focal_img
+        self.focal_obj = focal_obj
         self.theta_obj = theta_obj
-        self.k = cluster.material_data.k_b
+
+        if medium is None:
+            self.medium = miepy.materials.air()
+        else:
+            self.medium = medium
 
         self.theta = np.linspace(0, self.theta_obj, sampling)
         self.phi   = np.linspace(0, 2*np.pi, 2*sampling)
         self.THETA, self.PHI = np.meshgrid(self.theta, self.phi, indexing='ij')
 
-        n1 = n2 = self.cluster.medium.eps(2*np.pi/self.k)**0.5
+        self.n1 = self.cluster.medium.eps(cluster.wavelength)**0.5
+        self.n2 = self.medium.eps(cluster.wavelength)**0.5
+        self.k1 = 2*np.pi*self.n1/cluster.wavelength
+        self.k2 = 2*np.pi*self.n2/cluster.wavelength
 
         self.E_far = cluster.E_angular(self.THETA, self.PHI)
-        self.E_far *= np.sqrt(n1/n2)*np.sqrt(np.cos(self.THETA))
-        # self.E_far[1] *= np.cos(self.THETA)**2
+        self.magnification = self.n1*self.focal_img/(self.n2*self.focal_obj)
+        self.numerical_aperature = self.n1*np.sin(theta_obj)
 
     def image(self, x_array, y_array, z_val=0):
         """
@@ -36,21 +46,23 @@ class microscope:
             y_array    1D array of image y-values
             z_val      z-value of the image (relative to the focus)
         """
-        k = self.k
-        f = self.focal_length
-        factor = 1j*k*f*np.exp(-1j*k*f)/(2*np.pi)
+        k = self.k2
+        f1 = self.focal_obj
+        f2 = self.focal_img
+        factor = 1j*k*f2*np.exp(-1j*k*(f2+z_val))/(2*np.pi)*np.sqrt(self.n1/self.n2)*(f1/f2)**2 \
+                    *np.sin(self.THETA)*np.sqrt(np.cos(self.THETA)) \
+                    *np.exp(0.5j*k*z_val*(f1/f2)**2*np.sin(self.THETA)**2)
 
-        image = np.zeros((3, len(x_array), len(y_array)), dtype=complex)
+        image = np.zeros((2, len(x_array), len(y_array)), dtype=complex)
 
         for i,x in enumerate(tqdm(x_array)):
             for j,y in enumerate(y_array):
                 rho = np.sqrt(x**2 + y**2)
                 angle = np.arctan2(y, x)
-                angle = np.arctan2(x, y)
-                integrand = factor*self.E_far*np.exp(1j*k*z_val*np.cos(self.THETA))*np.exp(1j*k*rho*np.sin(self.THETA)*np.cos(self.PHI-angle))*np.sin(self.THETA)
+                integrand = factor*self.E_far*np.exp(1j*k*f1/f2*rho*np.sin(self.THETA)*np.cos(self.PHI-angle))
                 integrand = miepy.coordinates.vec_sph_to_cart(integrand, self.THETA, self.PHI)
 
-                for p in range(3):
+                for p in range(2):
                     image[p,i,j] = miepy.vsh.misc.trapz_2d(self.theta, self.phi, integrand[p])
         
         return image
